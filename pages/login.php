bf95834b9f492b60fa78a8d9bba7468ea1fa68ed
eudoradesir1 <144730292+eudoradesir1@users.noreply.github.com>
2026-08-03 11:06:30 -0400

    Update login.php
    added db integration so it will update the rows

--- pages/login.php
+++ pages/login.php
@@ -19,22 +19,45 @@
 
 <main>
     <section id="club_container">
-        <form id="org_login" method = "post">
-            <h1>Please log in to access your org's information...</h1>
-            
-            <!-- autocomplete possibly after certain amount of characters -->
-            <label for = "org">Affiliated Organization:</label><br>
-            <input type = text id = "org" required><br><br>
+       <form id="org_login" method="POST" action="login_process.php">
 
-            <!-- general body able type in org and press 'just view as general body' -->
-            <label for ="wID">WIT ID:</label><br>
-            <input type = "text" id = "wID" placeholder = "W000123456" pattern = "W\d{8}" required><br><br>
+    <h1>Please log in to access your org's information...</h1>
 
-            <label for ="password">Password:</label><br>
-            <input type = "password" id = "password" minLength = "4" required><br><br>
+    <label for="org">Affiliated Organization:</label><br>
+    <input 
+        type="text" 
+        id="org" 
+        name="organization"
+        required>
+    <br><br>
 
-            <button type ="button" onclick="auth()">Submit</button>
-         </form>
+
+    <label for="wID">WIT ID:</label><br>
+    <input 
+        type="text" 
+        id="wID"
+        name="wid"
+        placeholder="W000123456"
+        pattern="W\d{8}"
+        required>
+    <br><br>
+
+
+    <label for="password">Password:</label><br>
+    <input 
+        type="password"
+        id="password"
+        name="password"
+        minlength="4"
+        required>
+    <br><br>
+
+
+    <button type="submit">
+        Submit
+    </button>
+
+</form>
     </section>
 </main>
 </html>
